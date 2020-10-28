b33b15112c1e1a9e307d6b781fee7588fbe39baf
gaelleEK <elkadaouigaelle@gmail.com>
2020-10-28 14:00:32 +0100

    insertion fonction slug et autres maj

--- creer_article.php
+++ creer_article.php
@@ -1,21 +1,34 @@
 <?php
+/* appel fichier fonctions */
+require_once 'fonctions.php';
+
 /* connexion à la bdd */
 $pdo = new PDO('mysql:host=mysql;dbname=project-one;host=127.0.0.1', 'root', '', [
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
 ]);
+
+/* requete pour récuperer les catégories pour le select */
 $query = $pdo->query("SELECT * FROM category");
 $categories = $query->fetchAll();
 
-if (isset($_POST["title"])) {
+// si la variable post-title est affecté et n'est pas vide
+if (isset($_POST["title"])&& !empty($_POST["title"])
+    && isset($_POST["content"])&& !empty($_POST["content"])
+    && isset($_POST["category"])&& !empty($_POST["category"])) {
 
-    $title = $_POST["title"];
-    $content = $_POST["content"];
-    $slug = $_POST["slug"];
+        $title = $_POST["title"];
+        $content = $_POST["content"];
+        $category = $_POST["category"];
+        $slug = slugify($title);
 
-    $sql = "INSERT INTO article (title, content, slug) VALUES ('$title', '$content', '$slug')";
-    $pdo -> exec($sql);
-    $save = true;
+        $sql = "INSERT INTO article (title, content, createdAt, category_id, slug) VALUES ('$title', '$content', NOW(), '$category', '$slug')";
+        $pdo -> exec($sql);
+        $save = true;
 }
+
+
+
+
 ?>
 <!DOCTYPE html>
 <html lang="fr">
@@ -27,63 +40,47 @@ if (isset($_POST["title"])) {
 </head>
 <body>
 
-<?php if (!empty($save) && $save === true): ?>
-
-    <div class="alert alert-success">Article enregistré</div>
-
-
+<?php if (isset($message)&& $message === true): ?>
+    <div class="alert alert-success">article enregistré</div>
+<?php else: ?>
+    <div class="alert alert-danger">vérifez votre saisie</div>
 <?php endif; ?>
-<main class="container">
-    <div class="row">
-        <section class="col-12">
-
-            <form class="text-center" method="post">
 
 
+    <main class="container">
+        <div class="row">
+            <section class="col-12">
+                <form class="text-center" method="post">
+                    <div class="shadow p-3 mb-5 mt-5  rounded"   style="background-color: #4b4b4b">
 
-                <div class="shadow p-3 mb-5 mt-5  rounded"   style="background-color: #4b4b4b">
-                    <div class="mt-3 p-2" >
-
-                        <h2 class=" font-weight-bold ">Titre</h2>
-
-                        <div >
-                            <input class="form-control  "  name="title"></input>
+                        <div class="mt-3 p-2" >
+                                <h2 class=" font-weight-bold ">Titre</h2>
+                                <div >
+                                    <input class="form-control  "  name="title"></input>
+                                </div>
                         </div>
 
-                    </div>
-
-                    <div class="mt-3 p-2" >
-                        <h2 class=" font-weight-bold">Contenu</h2>
-                        <div class="input-group">
-                            <textarea name="content" class="form-control" aria-label="With textarea"></textarea>
+                        <div class="mt-3 p-2" >
+                            <h2 class=" font-weight-bold">Contenu</h2>
+                            <div class="input-group">
+                                <textarea name="content" class="form-control" aria-label="With textarea"></textarea>
+                            </div>
                         </div>
-                    </div>
 
-                    <div class="mt-3  p-2 ">
-                        <h2 class="  font-weight-bold">Categorie</h2>
-                        <div >
-                            <select class="form-control " >
+                        <div class="mt-3  p-2 ">
+                            <h2 class="  font-weight-bold">Categorie</h2>
+                            <select class="form-control" name="category">
                                 <?php foreach ($categories as $category): ?>
-                                    <option><?= $category["category_name"] ?></option>
+                                    <option value = "<?=$category["id"] ?>"> <?= $category["category_name"] ?></option>
                                 <?php endforeach; ?>
                             </select>
                         </div>
                     </div>
-                </div>
-                <button class=" btn btn-dark m-3 " type="submit" value="enregistrer">Enregistrer</button>
-
-
-    </div>
-
-    </div>
-
-
-
-    </form>
-
-    </section>
-    </div>
-</main>
+                    <button class=" btn btn-dark m-3 " type="submit" value="enregistrer">Enregistrer</button>
+                </form>
+            </section>
+        </div>
+    </main>
 
 </body>
 </html>
