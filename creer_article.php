b3279e88c8eddd55af9e799d674c3d0a22b1fec3
gerald <geraldandriot@yahoo.fr>
2020-10-28 09:27:23 +0100

    css complet
    foreach dans le select a ajouter

--- creer_article.php
+++ creer_article.php
@@ -2,7 +2,7 @@
 /* connexion à la bdd */
 $pdo = new PDO('mysql:host=mysql;dbname=project-one;host=127.0.0.1', 'root', '', [
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
-    ]);
+]);
 
 if (isset($_POST["title"])) {
 
@@ -20,32 +20,68 @@ if (isset($_POST["title"])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="style.css">
     <title>Créer un article</title>
 </head>
 <body>
-<!-- If permettant d'afficher une alerte si l'article est bien entré en bdd -->
+
 <?php if (!empty($save) && $save === true): ?>
-<div class="alert alert-success">Article enregistré</div>
+
+    <div class="alert alert-success">Article enregistré</div>
+
+
 <?php endif; ?>
+<main class="container">
+    <div class="row">
+        <section class="col-12">
+
+            <form class="text-center" method="post">
 
 
-    <form method="post">
-    <div class="form-group">
-    <label>Titre</label>
-    <input name="title"></input>
 
-    <label>Contenu</label>
-    <input name="content"></input>
+                <div class="shadow p-3 mb-5 mt-5  rounded"   style="background-color: #4b4b4b">
+                    <div class="mt-3 p-2" >
 
-    <label>Slug</label>
-    <input name="slug"></input>
+                        <h2 class=" font-weight-bold ">Titre</h2>
 
-    <button type="submit">Enregistrer</button>
+                        <div >
+                            <input class="form-control  "  name="title"></input>
+                        </div>
+
+                    </div>
+
+                    <div class="mt-3 p-2" >
+                        <h2 class=" font-weight-bold">Contenu</h2>
+                        <div class="input-group">
+                            <textarea name="content" class="form-control" aria-label="With textarea"></textarea>
+                        </div>
+                    </div>
+
+                    <div class="mt-3  p-2 ">
+                        <h2 class="  font-weight-bold">Categorie</h2>
+                        <div >
+                            <select class="form-control " >
+                                <!-- foreach -->
+                                <option><!-- option-->
+                                </option>
+                            </select>
+                        </div>
+                    </div>
+                </div>
+                <button class=" btn btn-dark m-3 " type="submit" value="enregistrer">Enregistrer</button>
+
+
     </div>
-    </form>
-    
 
+    </div>
+
+
+
+    </form>
+
+    </section>
+    </div>
+</main>
 
 </body>
 </html>
