0c2e1fa526871f913d1f043dd8bb7ce3ff1b2698
gerald <geraldandriot@yahoo.fr>
2020-10-28 11:20:56 +0100

    renomage fichies
    renomage des fichier avec le footer

--- accueil.php
+++ accueil.php
@@ -15,7 +15,7 @@
 <body>
 <img src="img/code1.jpg" class="img-fluid">
 <nav class="navbar navbar-expand-lg navbar navbar-dark " >
-        <a class="navbar-brand" href="accueil.html">Accueil</a>
+        <a class="navbar-brand" href="accueil.php">Accueil</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
---
+++ footer.php
@@ -0,0 +1,15 @@
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="style.css">
+        <title>Document</title>
+    </head>
+    <body>
+    <footer class="text-center text-white"><hr class="bg-dark">
+        Projet réalisé par Gaelle et Gerald 2020
+    </footer>
+    </body>
+</html>
