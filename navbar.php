a4b71d92d437d6a0bbcbc6c9f306caa7dea0b110
gaelleEK <elkadaouigaelle@gmail.com>
2020-11-02 09:12:56 +0100

    debut select navbar et lien

--- navbar.php
+++ navbar.php
@@ -1,4 +1,15 @@
+<?php
+/* connexion à la bdd */
+$pdo = new PDO('mysql:host=mysql;dbname=project-one;host=127.0.0.1', 'root', '', [
+PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+]);
 
+/* requête */
+$query = $pdo->query("SELECT * FROM category");
+
+$categories = $query->fetchAll();
+
+?>
 <img src="img/code1.jpg" class="img-fluid">
 
 <nav class="navbar navbar-expand-lg navbar navbar-dark " >
@@ -9,7 +20,18 @@
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav">
                 <li class="nav-item mr-3 ml-3">
-                    <a class="nav-link" href="page1.php">Article</a>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="page1.php" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Article
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="page1.php">Tous les articles</a>
+                        <div class="dropdown-divider"></div>
+                        <?php foreach ($categories as $category): ?>
+                        <a class="dropdown-item" href="page6.php?<?= $category["id"] ?>"><?= $category["category_name"] ?></a>
+                        <?php endforeach; ?>
+                    </div>
+                   <!-- <a class="nav-link" href="page1.php">Article</a>-->
                 </li>
                 <li class="nav-item mr-3">
                     <a class="nav-link" href="page2.php">Creer article</a>
